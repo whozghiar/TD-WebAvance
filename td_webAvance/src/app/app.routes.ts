c3d8177f29f26845d5ed66597b5e30b7f9ffa354
Irlando-MSI <73164809+whozghiar@users.noreply.github.com>
2024-03-19 12:23:24 +0100

    Routes & Navbar

--- td_webAvance/src/app/app.routes.ts
+++ td_webAvance/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LightpageComponent} from "./component/lightpage/lightpage.component";
+import {RacineComponent} from "./component/racine/racine.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'lights',component: LightpageComponent },
+  { path: 'racine',component: RacineComponent},
+  { path: '**',redirectTo: 'lights'}
+];
---
+++ td_webAvance/src/app/component/racine/racine.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-racine',
+  standalone: true,
+  imports: [],
+  templateUrl: './racine.component.html',
+  styleUrl: './racine.component.css'
+})
+export class RacineComponent {
+
+}
