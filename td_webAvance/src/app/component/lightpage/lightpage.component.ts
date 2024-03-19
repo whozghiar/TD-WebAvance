6aeda641ce5dcb362391ffc18f0ae67bbe3dd121
Irlando-MSI <73164809+whozghiar@users.noreply.github.com>
2024-03-19 10:49:23 +0100

    Création d'une lightpage

---
+++ td_webAvance/src/app/component/lightpage/lightpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lightpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './lightpage.component.html',
+  styleUrl: './lightpage.component.css'
+})
+export class LightpageComponent {
+
+}
