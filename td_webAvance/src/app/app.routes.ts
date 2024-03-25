85de256eae072ee6c26000982d14c655e879319f
Irlando-MSI <73164809+whozghiar@users.noreply.github.com>
2024-03-25 11:00:34 +0100

    - Mis en place des appels réseaux pour récupérer & ajouter une lampe sur notre API (Java)

--- td_webAvance/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {LightpageComponent} from "./component/lightpage/lightpage.component";
-import {RacineComponent} from "./component/racine/racine.component";
-
-export const routes: Routes = [
-  { path: 'lights',component: LightpageComponent },
-  { path: 'racine',component: RacineComponent},
-  { path: '**',redirectTo: 'lights'}
-];
